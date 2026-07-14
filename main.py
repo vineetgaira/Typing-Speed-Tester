ae2507ab40385c34e9a543e0a52132bb4dc8b959
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-14 08:58:11 +0530

    Add user input

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from src.passage import next_passage
 from src.display import welcome,show_menu,show_passage
 from src.game import select_difficulty
+from src.typing import get_user_input
 
 def main():
-    select_difficulty()
-
+    passage=next_passage()
+    show_passage(passage)
+    get_user_input(passage)
 
 if __name__ == "__main__":
     main()
--- src/typing.py
+++ src/typing.py
@@ -1,5 +1,11 @@
-def get_user_input():
-    pass
+import textwrap
+
+def get_user_input(passage):
+    user_input=input("Enter the above passage:")
+    if len(user_input)>len(passage):
+        print("You have wrote the whole passage.")
+    return user_input
+        
 
 def compare_text():
     pass
