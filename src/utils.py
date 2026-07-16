606763d419062b77aed4d965185e11fae79c332a
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-16 08:11:59 +0530

    Implement pause

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,12 @@
 import os
+from colorama import Fore, Style
 
 def clear_screen():
    
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def pause():
-    pass
+    pause=input(Fore.CYAN+Style.BRIGHT+"Press enter to continue..."+Style.RESET_ALL)
 
 def exit_program():
    pass
