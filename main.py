22b48f9fb54d1f1cea6fc074f3e070d3a28a3610
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-17 16:50:59 +0530

    Removed unnecessary comments

--- main.py
+++ main.py
@@ -7,15 +7,3 @@ def main():
   
 if __name__ == "__main__":
     main()
-
-"""
-Things I have to build..
---First I have to build a play_game() loop.
---I have to test the whole code.
---I might have to fix some bugs and extra code.
---Check if every feature works.
---I have to do state management.
---Updated highscore, Highscores and History save in a .json file.
---I need a dictionary for state management.
-
-"""
